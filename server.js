34f0b0f27ddd82ec3386f6eab7f406745b0d01be
dunlok <myloc442@gmail.com>
2023-04-17 17:16:05 +0700

    Route, More Route

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const app = express();
-const port = 8081;
+const express = require("express"); // commonjs
+const app = express(); // app of express
+const port = 8081; // port
 
+// route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/home", (req, res) => {
+  res.send("Home page");
+});
+
+// start
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
